91f6e1a9527bfc92ef14118e9dcc415fecd2e970
PriyanshuM2003 <studyx222@gmail.com>
2023-12-24 00:02:55 +0530

    updated

--- pages/streaming/[slug].js
+++ pages/streaming/[slug].js
@@ -2,7 +2,7 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 
-export default function Stream() {
+export default function Page() {
   const router = useRouter();
   return <p>Post: {router.query.slug}</p>;
 }
