233ba76769a9e315ea7c0d77ee318b75e439c5ec
PriyanshuM2003 <studyx222@gmail.com>
2024-02-11 15:36:57 +0530

    updated

--- pages/api/golive.js
+++ pages/api/golive.js
@@ -55,7 +55,7 @@ export default async function handler(req, res) {
           slug,
           hostUserId,
           participants: {
-            create: guestUserIds.map((guestUserId) => ({
+            create: guestUserIds.map((guestUserId, index) => ({
               guestUserId,
               uniqueToken: participants[index].uniqueToken,
             })),
