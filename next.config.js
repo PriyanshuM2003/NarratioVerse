54ee74d20089fc132d335d98ac173a4e9b0ec5d0
PriyanshuM2003 <studyx222@gmail.com>
2024-02-11 15:10:21 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -3,27 +3,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn-icons-png.flaticon.com",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "dummyimage.com",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "pluspng.com",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "supabase.co",
+        hostname: "**",
+        port: "",
         pathname: "**",
       },
     ],
