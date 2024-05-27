ca4aa9366ad1d0111c393744a4ecc085ef0e9fe7
PriyanshuM2003 <studyx222@gmail.com>
2024-05-27 17:36:37 +0530

    updated

--- pages/plans/checkout/success.tsx
+++ pages/plans/checkout/success.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 
 const Success = () => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.location.replace("/");
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24 text-4xl">
       Order Completed
